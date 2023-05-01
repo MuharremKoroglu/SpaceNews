b8adc683d619fb51740be95c7981615ecc1a5b1b
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-05-01 12:53:20 +0300

    Initial Commit

---
+++ SpaceNews/SpaceNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceNewsApp.swift
+//  SpaceNews
+//
+//  Created by Muharrem Köroğlu on 1.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
